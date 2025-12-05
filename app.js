58394701bef7a5d4fc5180838b4e6339f86d659b
kldwnn <23011063@myrp.edu.sg>
2025-12-05 14:21:00 +0800

    exception records updated

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const XLSX = require('xlsx');
-const db = require('./db');
 const session = require('express-session');
 
 const app = express();
